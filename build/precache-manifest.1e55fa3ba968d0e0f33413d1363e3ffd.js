59e0d0846cd620b843c9303adc5d9026c59b0f73
Markus Randa <randamarkus.95@gmail.com>
2020-05-25 14:05:14 +0200

    Added production build

---
+++ build/precache-manifest.1e55fa3ba968d0e0f33413d1363e3ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f01ff076f96f67b73ad0a4b811f608a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "829688ecdf56f5ba0c2f",
+    "url": "/static/css/main.cd1fea08.chunk.css"
+  },
+  {
+    "revision": "d136c824a7595a504f5a",
+    "url": "/static/js/2.1025537c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1025537c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829688ecdf56f5ba0c2f",
+    "url": "/static/js/main.3959fe55.chunk.js"
+  },
+  {
+    "revision": "d72f0fe1f3f2f095774c",
+    "url": "/static/js/runtime-main.8813442d.js"
+  }
+]);
