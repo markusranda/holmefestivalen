c8f735ad9cc4a44d96023bccf476c571128b5332
Markus Randa <randamarkus.95@gmail.com>
2020-05-26 06:51:19 +0200

    Dockerized

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-const hostname = "127.0.0.1";
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname)));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server listening on ${port}/`);
 });
